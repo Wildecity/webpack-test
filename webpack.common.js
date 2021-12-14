ff5765088eee2e47945dfb96cba1767e55083b36
GuidoRial <rialguido@gmail.com>
2021-12-14 17:36:32 -0300

    added the basic style of the webpage

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
+module.exports = {
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
