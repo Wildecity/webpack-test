29ed6247c7f7d2917db039c382c53370dc0e3915
Wildecity <81671271+Wildecity@users.noreply.github.com>
2021-12-15 20:12:41 -0300

    products.js added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { myFunction } from './about.js';
-import { createHome } from './home.js';
+import { myFunction } from "./about.js";
+import { createHome } from "./home.js";
+import { createProducts } from "./products.js";
 
 myFunction();
 
-createHome();
+// createHome();
+createProducts();
